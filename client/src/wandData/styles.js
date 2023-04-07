89b47ef9876886e154490359ff9e9aad52013d59
brenda <115198624+brenda255@users.noreply.github.com>
2023-04-06 22:02:26 -0600

    added style wands

---
+++ client/src/wandData/styles.js
@@ -0,0 +1,10 @@
+export const styles = [
+    { label: 'Classic', src: '../images/classic.png' },
+    { label: 'Crooked Spiral', src: '../images/crooked-spiral.png' },
+    { label: 'Natural', src: '../images/natural.png' },
+    { label: 'Notched', src: '../images/notched.png' },
+    { label: 'Ringed', src: '../images/ringed.png' },
+    { label: 'Soft Spiral', src: '../images/soft-spiral.png' },
+    { label: 'Spiral', src: '../images/spiral.png' },
+    { label: 'Stalk', src: '../images/stalk.png' },
+];
