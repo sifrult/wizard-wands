69c0d48ccc5a7bb7a06ea1ae41421f7ef0aad655
Kate Steed <sunfiresteed@yahoo.com>
2023-04-10 21:46:36 -0600

    added soft-spiral, spiral, and stalk.js

--- client/src/pages/Store.js
+++ client/src/pages/Store.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import StyleGallery from '../components/StyleGallery';
 import { ClassicWands } from '../wandData/classic';
 import { naturalWands } from '../wandData/natural';
+import { SoftSpiralWands } from '../wandData/soft-spiral';
+import { SpiralWands } from '../wandData/spiral';
+import { StalkWands } from '../wandData/stalk';
+
 import Aside from './Aside';
 
 export default function Store() {
@@ -22,12 +26,36 @@ export default function Store() {
     setWandStyle(wands);
     setWand(naturalWands[0]);
   };
+  const softSpiralHandler = () => {
+    const wands = SoftSpiralWands.map((wand) => (
+      <StyleGallery key={wand.label} wand={wand} setWand={setWand} />
+    ));
+    setWandStyle(wands);
+    setWand(SoftSpiralWands[0]);
+  };
+  const spiralHandler = () => {
+    const wands = SpiralWands.map((wand) => (
+      <StyleGallery key={wand.label} wand={wand} setWand={setWand} />
+    ));
+    setWandStyle(wands);
+    setWand(SpiralWands[0]);
+  };
+  const stalkHandler = () => {
+    const wands = StalkWands.map((wand) => (
+      <StyleGallery key={wand.label} wand={wand} setWand={setWand} />
+    ));
+    setWandStyle(wands);
+    setWand(StalkWands[0]);
+  };
 
   return (
     <>
       <p>Store</p>
       <div onClick={classicHandler}>Classic</div>
       <div onClick={naturalHandler}>Natural</div>
+      <div onClick={softSpiralHandler}>Soft Spiral</div>
+      <div onClick={spiralHandler}>Spiral</div>
+      <div onClick={stalkHandler}>Stalk</div>
 
       {wandStyle}
       {/* {wand && <div>{wand.label}</div>} */}
