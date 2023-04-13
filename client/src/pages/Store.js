9a3b5eb8b00e0a94ed54f74db4a064dd3def0eb0
Amy Snarr <amy.snarr16@gmail.com>
2023-04-12 20:21:09 -0600

    added the colors to all the wands

--- client/src/pages/Store.js
+++ client/src/pages/Store.js
@@ -14,67 +14,118 @@ export default function Store() {
   const [wand, setWand] = useState('');
   const [wandStyle, setWandStyle] = useState([]);
   const [labelName, setLabelName] = useState('');
+  const [hoveredWandLabel, setHoveredWandLabel] = useState(null);
 
   const classicHandler = () => {
     const wands = classicWands.map((wand) => (
-      <StyleGallery key={wand.label} wand={wand} setWand={setWand} setLabelName={setLabelName}/>
+     <StyleGallery
+        key={wand.label}
+        wand={wand}
+        setWand={setWand}
+        setLabelName={setLabelName}
+        hoveredWandLabel={hoveredWandLabel}
+        setHoveredWandLabel={setHoveredWandLabel}
+      />
     ));
     setWandStyle(wands);
     setWand(classicWands[0]);
   };
 
   const naturalHandler = () => {
-    const wands = naturalWands.map((goop) => (
-      <StyleGallery key={goop.color} wand={goop} setWand={setWand} setLabelName={setLabelName}/>
+    const wands = naturalWands.map((wand) => (
+      <StyleGallery
+      key={wand.label}
+      wand={wand}
+      setWand={setWand}
+      setLabelName={setLabelName}
+      hoveredWandLabel={hoveredWandLabel}
+      setHoveredWandLabel={setHoveredWandLabel}
+    />
     ));
     setWandStyle(wands);
     setWand(naturalWands[0]);
   };;
 
   const notchedHandler = () => {
-    const wands = notchedWands.map((goop) => (
-      <StyleGallery key={goop.color} wand={goop} setWand={setWand} setLabelName={setLabelName}/>
-    ));
+    const wands = notchedWands.map((wand) => (
+      <StyleGallery
+      key={wand.label}
+      wand={wand}
+      setWand={setWand}
+      setLabelName={setLabelName}
+      hoveredWandLabel={hoveredWandLabel}
+      setHoveredWandLabel={setHoveredWandLabel}
+    />    ));
     setWandStyle(wands);
     setWand(notchedWands[0]);
   };
 
   const ringedHandler = () => {
-    const wands = ringedWands.map((goop) => (
-      <StyleGallery key={goop.color} wand={goop} setWand={setWand} setLabelName={setLabelName}/>
-    ));
+    const wands = ringedWands.map((wand) => (
+      <StyleGallery
+      key={wand.label}
+      wand={wand}
+      setWand={setWand}
+      setLabelName={setLabelName}
+      hoveredWandLabel={hoveredWandLabel}
+      setHoveredWandLabel={setHoveredWandLabel}
+    />    ));
     setWandStyle(wands);
     setWand(ringedWands[0]);
   };
 
   const softSpiralHandler = () => {
-    const wands = softSpiralWands.map((goop) => (
-      <StyleGallery key={goop.color} wand={goop} setWand={setWand} setLabelName={setLabelName}/>
-    ));
+    const wands = softSpiralWands.map((wand) => (
+      <StyleGallery
+      key={wand.label}
+      wand={wand}
+      setWand={setWand}
+      setLabelName={setLabelName}
+      hoveredWandLabel={hoveredWandLabel}
+      setHoveredWandLabel={setHoveredWandLabel}
+    />    ));
     setWandStyle(wands);
     setWand(softSpiralWands[0]);
   };
 
   const spiralHandler = () => {
-    const wands = spiralWands.map((goop) => (
-      <StyleGallery key={goop.color} wand={goop} setWand={setWand} setLabelName={setLabelName}/>
-    ));
+    const wands = spiralWands.map((wand) => (
+      <StyleGallery
+      key={wand.label}
+      wand={wand}
+      setWand={setWand}
+      setLabelName={setLabelName}
+      hoveredWandLabel={hoveredWandLabel}
+      setHoveredWandLabel={setHoveredWandLabel}
+    />    ));
     setWandStyle(wands);
     setWand(spiralWands[0]);
   };
 
   const stalkHandler = () => {
-    const wands = stalkWands.map((goop) => (
-      <StyleGallery key={goop.color} wand={goop} setWand={setWand} setLabelName={setLabelName}/>
-    ));
+    const wands = stalkWands.map((wand) => (
+      <StyleGallery
+      key={wand.label}
+      wand={wand}
+      setWand={setWand}
+      setLabelName={setLabelName}
+      hoveredWandLabel={hoveredWandLabel}
+      setHoveredWandLabel={setHoveredWandLabel}
+    />    ));
     setWandStyle(wands);
     setWand(stalkWands[0]);
   };
 
   const crookedSpiralHandler = () => {
-    const wands = crookedSpiralWands.map((goop) => (
-      <StyleGallery key={goop.color} wand={goop} setWand={setWand} setLabelName={setLabelName}/>
-    ));
+    const wands = crookedSpiralWands.map((wand) => (
+      <StyleGallery
+      key={wand.label}
+      wand={wand}
+      setWand={setWand}
+      setLabelName={setLabelName}
+      hoveredWandLabel={hoveredWandLabel}
+      setHoveredWandLabel={setHoveredWandLabel}
+    />    ));
     setWandStyle(wands);
     setWand(crookedSpiralWands[0]);
   };
@@ -90,8 +141,8 @@ export default function Store() {
       <div onClick={softSpiralHandler}>Soft Spiral</div>
       <div onClick={spiralHandler}>Spiral</div>
       <div onClick={stalkHandler}>Stalk</div>
-    <div>{wandStyle}</div>
-    <p>Label value: {labelName}</p>
+    <div style={{display:'flex', flexWrap: 'wrap'}}>{wandStyle}</div>
+    <p>Label value: {hoveredWandLabel || labelName}</p>
 
       <div>
           <img src={wand?.src || ''} alt={wand?.label || ''} />
