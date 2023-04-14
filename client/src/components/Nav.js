1ff7b8d7d52e8ee61c839be3b3646714768f90c9
Kate Steed <sunfiresteed@yahoo.com>
2023-04-13 19:41:43 -0600

    updated nav bar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import Auth from "../utils/auth";
+import { Link } from "react-router-dom";
 
 function Nav() {
-    return (
-        <ul>
-            <li>
-                <Link to='/WelcomePage'>
-                    Welcome
-                </Link>
-            </li>
-            <li>
-                <Link to='/Hello'>
-                    Hello
-                </Link>
-            </li>
-            <li>
-                <Link to='/Store'>
-                    Store
-                </Link>
-            </li>
+
+  function showNavigation() {
+    if (Auth.loggedIn()) {
+      return (
+        <ul className="flex-row">
+          <li>
+            <Link to="/Store">
+              Store
+            </Link>
+          </li>
+          <li>
+            <Link to="/Cart">
+              Cart (NOT WORKING YET)
+            </Link>
+          </li>
+          <li>
+            <a href="/" onClick={() => Auth.logout()}>
+              Logout
+            </a>
+          </li>
         </ul>
-    )
+      );
+    } else {
+      return (
+        <ul>
+          <li>
+            <Link to="/Store">
+              Store
+            </Link>
+          </li>
+          <li>
+            <Link to="/Login">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/signup">
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+      );
+    }
+  }
+
+  return (
+    <header>
+      <h1>
+          Ollisander's
+      </h1>
+
+      <nav>
+        {showNavigation()}
+      </nav>
+    </header>
+  );
 }
 
 export default Nav;
