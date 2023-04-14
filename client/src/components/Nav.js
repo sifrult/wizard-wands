7a0e5f30c8f6ebf2951c416cd86cdfa5c62b9c9f
Kate Steed <sunfiresteed@yahoo.com>
2023-04-13 20:08:55 -0600

    added mui components to package.json

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,29 +1,66 @@
 import React from "react";
 import Auth from "../utils/auth";
+import {
+    AppBar,
+    Toolbar,
+    CssBaseline,
+    Typography,
+    makeStyles,
+  } from "@material-ui/core";
 import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+    navlinks: {
+      marginLeft: theme.spacing(10),
+      display: "flex",
+    },
+   logo: {
+      flexGrow: "1",
+      cursor: "pointer",
+    },
+    link: {
+      textDecoration: "none",
+      color: "white",
+      fontSize: "20px",
+      marginLeft: theme.spacing(20),
+      "&:hover": {
+        color: "yellow",
+        borderBottom: "1px solid white",
+      },
+    },
+  }));
 
 function Nav() {
 
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <ul className="flex-row">
-          <li>
-            <Link to="/Store">
-              Store
-            </Link>
-          </li>
-          <li>
-            <Link to="/Cart">
-              Cart (NOT WORKING YET)
-            </Link>
-          </li>
-          <li>
-            <a href="/" onClick={() => Auth.logout()}>
-              Logout
-            </a>
-          </li>
-        </ul>
+        <AppBar position="static">
+            <CssBaseline />
+            <Toolbar>
+                <Typography variant="h6" component="div" className={classes.navlinks}>
+                    Ollisander's
+                </Typography>
+            </Toolbar>
+
+        </AppBar>
+        // <ul className="flex-row">
+        //   <li>
+        //     <Link to="/Store">
+        //       Store
+        //     </Link>
+        //   </li>
+        //   <li>
+        //     <Link to="/Cart">
+        //       Cart (NOT WORKING YET)
+        //     </Link>
+        //   </li>
+        //   <li>
+        //     <a href="/" onClick={() => Auth.logout()}>
+        //       Logout
+        //     </a>
+        //   </li>
+        // </ul>
       );
     } else {
       return (
