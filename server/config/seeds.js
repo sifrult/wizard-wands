6be725dfaf63266a1052b88b9af8538b40f07306
Eli Cadena <103008210+ecadena7@users.noreply.github.com>
2023-04-15 13:12:01 -0600

    created users

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -6,42 +6,76 @@ db.once('open', async () => {
 
     const products = await Product.insertMany([
         {
+            // Bellatrix L.
             core:'Dragon Heart String',
             flexibility: 'Unyielding',
             length: 12.75,
             woodtype: 'Walnut',
-            wandtype: 'Stalk',
+            wandtype: 'crookedSpiral',
         },
         {
+            // Prof. McGonagall's Wand
+            core:'Dragon Heart String',
+            flexibility: 'Stiff',
+            length: 10.75,
+            woodtype: 'Fir',
+            wandtype: 'classic',
+        },
+        {
+            // Ginny's wand
             core:'Unicorn hair',
-            flexibility: 'Swishy',
+            flexibility: 'Quite Flexible',
             length: 14.25,
             woodtype: 'Yew',
             wandtype: 'softSpiral',
         },
         {
+            // Ron's wand #2
+            core:'Unicorn hair',
+            flexibility: 'Quite Bendy',
+            length: 14.0,
+            woodtype: 'Willow',
+            wandtype: 'softSpiral',
+        },
+        {
+            // elderWand
             core:'Unicorn Hair',
-            flexibility: 'Slightly Yielding',
+            flexibility: 'Stiff',
             length: 9.5,
             woodtype: 'Elder',
-            wandtype: 'Spiral',
+            wandtype: 'stalk',
         }
     ]);
 
     console.log('products seeded');
 
     await User.create({
-        username: '',
+        //Molly Weasley
+        username: 'Knittingwiz',
+        password: 'password12345',
         orders: [
             {
-                product: [products[0]._id, products[1]._id, products[2]._id]
+                product: [products[3]._id, products[4]._id]
             }
         ]
     });
 
     await User.create({
-        username: '',
-        password: '',
+        // Bellatrix
+        username: 'DeathEater1',
+        password: 'password12345',
+        orders: [{
+            products: [products[0]._id]
+        }]
+    });
+
+    await User.create({
+        //Professor McGonagall
+        username: 'tabbyCat',
+        password: 'password12345',
+        orders: [{
+            products: [products[2]._id]
+        }]
     });
 
     console.log('users seeded');
