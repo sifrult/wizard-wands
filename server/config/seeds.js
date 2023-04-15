c7a64e1e1ae012cec1f6a532ed5f8c3bcaacc852
Eli Cadena <103008210+ecadena7@users.noreply.github.com>
2023-04-15 12:27:00 -0600

    created product seeds. pending users and orders

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,2 +1,50 @@
 const db = require('./connection');
-const { Wand } = require('../models')
+const { User, Product } = require('../models')
+
+db.once('open', async () => {
+    await Product.deleteMany();
+
+    const products = await Product.insertMany([
+        {
+            core:'Dragon Heart String',
+            flexibility: 'Unyielding',
+            length: 12.75,
+            woodtype: 'Walnut',
+            wandtype: 'Stalk',
+        },
+        {
+            core:'Unicorn hair',
+            flexibility: 'Swishy',
+            length: 14.25,
+            woodtype: 'Yew',
+            wandtype: 'softSpiral',
+        },
+        {
+            core:'Unicorn Hair',
+            flexibility: 'Slightly Yielding',
+            length: 9.5,
+            woodtype: 'Elder',
+            wandtype: 'Spiral',
+        }
+    ]);
+
+    console.log('products seeded');
+
+    await User.create({
+        username: '',
+        orders: [
+            {
+                product: [products[0]._id, products[1]._id, products[2]._id]
+            }
+        ]
+    });
+
+    await User.create({
+        username: '',
+        password: '',
+    });
+
+    console.log('users seeded');
+
+    process.exit();
+});
