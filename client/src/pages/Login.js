e066ca10a9679329be2a3b9583ff8ad6029d5f5e
Styrlingjohn <styrling_morris@yahoo.com>
2023-04-16 17:14:03 -0600

    added styling to login and signup

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,6 +3,8 @@ import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
+import { Box, Button } from '@mui/material';
+import { UndoRounded } from '@mui/icons-material';
 
 function Login(props) {
     const [formState, setFormState] = useState({ username: '', password: '' });
@@ -24,47 +26,94 @@ function Login(props) {
     const handleChange = (event) => {
         const { name, value } = event.target;
         setFormState({
-          ...formState,
+            ...formState,
             [name]: value,
         });
     };
 
     return (
-        <div className="container">
-            First Time? <Link to="/signup">Sign Up!</Link>
+        <div className="container" style={{ fontFamily: 'aclonica' }}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'align-self',
+                    alignItems: 'center',
+                    justifyContent: 'space-evenly',
+                    height: '8vh',
+                    backgroundColor: '#bdbdbd',
+                    width: '80%',
+                    margin: 'auto',
+                    border: 1
 
-            <h2>Login</h2>
-            <form onSubmit={handleFormSubmit}>
-                <div className='flex-row space-between my-2'>
-                    <label htmlFor='username'>Username: </label>
-                    <input
-                    placeholder="username"
-                    name='username'
-                    type='username'
-                    id='username'
-                    onChange={handleChange}
-                    />
-                </div>
-                <div className='flex-row space-between my-2'>
-                    <label htmlFor='pwd'>Password: </label>
-                    <input
-                    placeholder="********"
-                    name='password'
-                    type='password'
-                    id='pwd'
-                    onChange={handleChange}
-                    />
-                </div>
-                {error ? (
-                    <div>
-                        <p className="error-text">Username or password is incorrect</p>
+                }}>
+                First Time?
+                <Link underline='none' to="/signup">
+                    <Button
+                        variant='contained'
+                        sx={{
+                            paddingTop: '5px',
+                            paddingBottom: '5px',
+                            backgroundColor: 'white',
+                            color: 'black',
+                        }}>Signup
+                    </Button>
+                </Link>
+            </Box>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '8vh',
+                    marginTop: '4vh',
+                    border: 1,
+                    paddingBottom: '6vh',
+                    paddingTop: '4vh',
+                    backgroundColor: '#bdbdbd',
+
+                }}>
+                <h2>Login</h2>
+                <form onSubmit={handleFormSubmit}>
+                    <div className='flex-row space-between my-2'>
+                        <label htmlFor='username'>Username: </label>
+                        <input
+                            placeholder="username"
+                            name='username'
+                            type='username'
+                            id='username'
+                            onChange={handleChange}
+                        />
                     </div>
+                    <div className='flex-row space-between my-2'>
+                        <label htmlFor='pwd'>Password: </label>
+                        <input
+                            placeholder="********"
+                            name='password'
+                            type='password'
+                            id='pwd'
+                            onChange={handleChange}
+                        />
+                    </div>
+                    {error ? (
+                        <div>
+                            <p className="error-text">Username or password is incorrect</p>
+                        </div>
                     ) : null}
                     <div className='flex-row flex-end'>
-                        <button type='submit'>Submit</button>
-                    </div>       
-            </form>
+                        <Button variant='contained'
+                            sx={{
+                                paddingTop: '1px',
+                                paddingBottom: '1px',
+                                backgroundColor: 'white',
+                                type: 'submit',
+                                color: 'black'
+                            }}>Submit</Button>
+                    </div>
+                </form>
+            </Box>
         </div>
+
     );
 }
 
