26859975ce5f04345907255d2df33d4c5b2647fa
Amy Snarr <amy.snarr16@gmail.com>
2023-09-16 21:19:28 -0600

    env files

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/wizardWandStore', {
+mongoose.connect(process.env.MONGO_URL || 'mongodb://mongo:vFiWGhFJgn62BmGASDp6@containers-us-west-84.railway.app:8016', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
